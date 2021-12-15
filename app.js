59513b028af519bb789a7f4d172bee4eac6fa040
sigsant <sigfridsantamera@hotmail.com>
2021-12-15 16:15:00 +0100

    remove unnecesary console.log calls

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ hbs.registerPartials('./templates/partials')
 
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname,('public/index.html')))
-// })
-
 app.get('/', (req, res) => {
     res.render('index', {});
 })
