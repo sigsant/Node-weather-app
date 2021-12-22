0feb8a522995d855eb786d3f0087f46ae329d22c
sigsant <sigfridsantamera@hotmail.com>
2021-12-22 22:52:06 +0100

    test invalid data query

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -6,10 +6,14 @@ const HomeLink = 'a:has-text("Home")'
 test.describe("Index: ", () => {
 	test.beforeEach( async ({ page }) => {
 		await page.goto("https://heros-weather-app.herokuapp.com/");
+		// await page.goto("http://localhost:3000");
+
 	})
 	test("Link About redirect to About page", async({ page }) => {
 		await page.click(AboutLink);
 		await expect(page).toHaveURL('https://heros-weather-app.herokuapp.com/about');
+		// await expect(page).toHaveURL('http://localhost:3000/about');
+
 	});
 	test("Show Lisboa title after filling the form", async({ page }) => {
 		await page.fill('input', 'Lisboa');
@@ -19,6 +23,11 @@ test.describe("Index: ", () => {
 	test("Display error if doesn't exists query data", async({ page }) => {
 		await page.click('button');
 		await expect(page.locator('#info-error')).toHaveText("There is no address in the query");
+	})
+	test("display error if the query data is invalid", async({ page }) => {
+		await page.fill('input', '!');
+		await page.click('button');
+		await expect(page.locator('#info-error')).toHaveText("No location found. Please, perform another search");
 	})
 })
 
