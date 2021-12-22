11b1f4d0ff0df5b27b800deaaef110c90d2a9987
sigsant <sigfridsantamera@hotmail.com>
2021-12-22 22:07:02 +0100

    translate into english and fix error with invalid data query

--- dist/utils/geomap.js
+++ dist/utils/geomap.js
@@ -1,33 +1,33 @@
-"use strict";
-const request = require('postman-request');
+var request = require('postman-request');
 require('dotenv').config();
-const geomap_token = process.env.GEO_TOKEN;
+var geomap_token = process.env.GEO_TOKEN;
 /**
  * Geolocalización de un lugar, devolviendo las coordenadas
  *
  * @param address Nombre del lugar en inglés
  * @param callback
  */
-const geomap = (address, callback) => {
-    const geo_api = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${geomap_token}&limit=1`;
+var geomap = function (address, callback) {
+    var geo_api = "https://api.mapbox.com/geocoding/v5/mapbox.places/".concat(address, ".json?access_token=").concat(geomap_token, "&limit=1");
     request({
         url: geo_api,
         json: true
-    }, (error, response) => {
+    }, function (error, response) {
+        var features = response.body.features;
         if (error) {
-            callback("Imposible conectarse al servicio de MapBox", undefined);
+            callback("Unable to connect to Mapbox service", undefined);
         }
-        else if (response.body.features.length === 0 || response.statusCode != 200) {
-            callback("No se ha hallado ninguna localización. Realice otra búsqueda", response.statusCode);
+        else if (features.length === 0 || response.statusCode != 200) {
+            callback("No location found. Please, perform another search", response.statusCode);
         }
         else {
-            callback(undefined, {
-                longitud: response.body.features[0].center[0],
-                latitud: response.body.features[0].center[1],
-                localizacion: response.body.features[0].place_name
-            });
+            var geoData = {
+                longitud: features[0].center[0],
+                latitud: features[0].center[1],
+                localizacion: features[0].place_name
+            };
+            callback(undefined, geoData);
         }
     });
 };
 module.exports = geomap;
-//# sourceMappingURL=geomap.js.map
